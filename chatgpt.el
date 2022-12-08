0249367a340df4035318b12a0f5e7c583d345336
Hamza <hamza.h.hamud@gmail.com>
2022-12-08 01:06:54 +0000

    fix: auth bug

--- chatgpt.el
+++ chatgpt.el
@@ -24,7 +24,7 @@
 
 
 ;;;; Parameters
-(defconst chatgpt-user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+(defconst chatgpt-user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0"
   "User agent set for requests.")
 
 (defconst chatgpt-url-auth "https://chat.openai.com/api/auth/session"
@@ -36,8 +36,11 @@
 (defconst chatgpt-api "https://chat.openai.com/api"
   "ChatGPT URL API.")
 
-(defconst chatgpt-env "~/Documents/projects/upg-tool/.env"
+(defconst chatgpt-env "~/Documents/projects/chatgpt-mode/.env"
   "Env file destination.")
+
+(defvar chatgpt-auth-token ""
+  "Auth token.")
 
 ;;;; Functions
 (defun chatgpt-place-session-token (session-token)
@@ -77,36 +80,45 @@
     data))
 
 (defun chatgpt--auth-headers (session-token)
-  (let ((headers `(("cookie" . (format "__Secure-next-auth.session-token=%s" ,session-token))
-                   ("user-agent" . chatgpt-user-agent))))
+  (let ((headers `(("cookie" . ,(format "__Secure-next-auth.session-token=%s" session-token))
+                   ("user-agent" . ,chatgpt-user-agent))))
     headers))
 
-
-(defun chatgpt--json-read ()
-  (let* ((json-object-type `plist )
-       (json-read))))
-
-(defun chatgpt-construct-response (prompt)
+(defun chatgpt--fetch-auth-token ()
   (let* ((env (chatgpt--read-env-file chatgpt-env))
-        (session_token (cdr (assoc "SESSION_TOKEN" env))))
-  (request chatgpt-url
-  :type "POST"
-  :data (chatgpt--conversation-data prompt)
-  :headers (chatgpt--conversation-headers session_token)
-  :parser (chatgpt--json-read )
-  :success (cl-function
-            (lambda (&key data &allow-other-keys)
-              (message "I sent: %S" (assoc-default 'form data))))
-  :error
-  (cl-function
-          (lambda (&key data &allow-other-keys)
-            (print (format "Error: %s" data)))))))
+        (session-token (cdr (assoc "SESSION_TOKEN" env))))
+    (request chatgpt-url-auth
+      :type "GET"
+      :headers (chatgpt--auth-headers session-token)
+      :parser 'json-read
+      :success (cl-function
+                (lambda (&key data &allow-other-keys)
+                  (let ((auth-token (assoc-default 'accessToken data)))
+                   (setq chatgpt-auth-token auth-token))))
+      :error (cl-function
+                (lambda (&key data &allow-other-keys)
+                (print (format "Error: %s" data)))))))
 
+
+(defun chatgpt--construct-response (prompt)
+  (chatgpt--fetch-auth-token)
+  (request chatgpt-url-backend
+  :type "POST"
+  :data (json-encode (chatgpt--conversation-data prompt "1"))
+  :headers (chatgpt--conversation-headers chatgpt-auth-token)
+  :parser 'json-read
+  :success (cl-function
+            (lambda (&rest data)
+              (message "I sent: %S" data)))
+  :error (cl-function
+          (lambda (&rest data)
+            (print (format "Error: %S" data))))))
 
 (defun chatgpt-fetch-response (prompt)
   (interactive "sInput Prompt:")
-  (chatgpt-construct-response prompt))
+  (chatgpt--construct-response prompt))
 
+;; move these to README or the config file
 (global-set-key (kbd "C-c C-c b") 'chatgpt-fetch-response)
 (global-set-key (kbd "C-c C-c c") 'chatgpt-place-session-token)
 
